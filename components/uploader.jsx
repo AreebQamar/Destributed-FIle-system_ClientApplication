3515fe15895f2c73b0e13c44d693ab67d6e06643
m-ali-z <alizulfiqar22222@gmail.com>
2024-06-02 03:01:23 +0500

    File uploading successfully

--- components/uploader.jsx
+++ components/uploader.jsx
@@ -1,21 +1,58 @@
+import { useEffect, useRef, useState } from "react";
 import { MdFileUpload } from "react-icons/md";
+import LoadingSpinner from "./loading-spinner";
+export default function FileUploader() {
+  const [file, setFile] = useState("");
 
-export default function FileUploader(){
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0]);
+    console.log("File selected");
+  };
 
+  useEffect(() => {
+    if (file != "") {
+      uploadFile();
+    }
+  }, [file]);
 
-    return(
-        <div className="">
-        {/* File upload icon */}
-        <label htmlFor="fileInput" className="w-16  cursor-pointer flex items-center justify-center">
-            <MdFileUpload className="w-8 h-8 " />
-        </label>
-        {/* Hidden file input */}
-        <input
-            id="fileInput"
-            type="file"
-            multiple
-            className="hidden"
-        />
+  const uploadFile = async () => {
+    const formData = new FormData();
+    formData.append("file", file);
+    console.log(formData);
+    try {
+      const response = await fetch("http://localhost:4000/upload", {
+        method: "POST",
+        body: formData,
+      });
+
+      if (!response.ok) {
+        throw new Error("Upload failed");
+      }
+
+      const result = await response.text();
+      alert("File uploaded successfully");
+    } catch (error) {
+      console.log(error);
+      alert("Error uploading file");
+    }
+  };
+
+  return (
+    <div className="">
+      {/* File upload icon */}
+      <label
+        htmlFor="fileInput"
+        className="w-16  cursor-pointer flex items-center justify-center"
+      >
+        <MdFileUpload className="w-8 h-8 " />
+      </label>
+      {/* Hidden file input */}
+      <input
+        id="fileInput"
+        type="file"
+        className="hidden"
+        onChange={handleFileChange}
+      />
     </div>
-    )
+  );
 }
