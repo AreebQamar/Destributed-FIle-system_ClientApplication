da7d8819010d80e2f9015993644c777cfb134a91
m-ali-z <alizulfiqar22222@gmail.com>
2024-06-09 13:08:42 +0500

    Nothing

--- components/header.jsx
+++ components/header.jsx
@@ -1,21 +1,19 @@
-
 import { useState } from "react";
 import FileUploader from "./uploader";
+import NewFileUploader from "./newFileUploader";
 
-
-export default function Header({filterString, setfilterString}) {
-
-    return (
-        <div className="flex justify-center items-center space-x-3 mt-4">
-            <input
-                type="text"
-                placeholder="Search..."
-                id="search"
-                value={filterString}
-                onChange={(e) => (setfilterString(e.target.value))}
-                className="p-2 border rounded-md placeholder-gray-500 text-black"
-            />
-            <FileUploader />
-        </div>
-    )
+export default function Header({ filterString, setfilterString }) {
+  return (
+    <div className="flex justify-center items-center space-x-3 mt-4">
+      <input
+        type="text"
+        placeholder="Search..."
+        id="search"
+        value={filterString}
+        onChange={(e) => setfilterString(e.target.value)}
+        className="p-2 border rounded-md placeholder-gray-500 text-black"
+      />
+      <NewFileUploader />
+    </div>
+  );
 }
