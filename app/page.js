124dc24e12363d25bc4449d4d0f706c493eb9142
m-ali-z <alizulfiqar22222@gmail.com>
2024-06-09 13:03:57 +0500

    File Download

--- app/page.js
+++ app/page.js
@@ -1,17 +1,15 @@
-"use client"
+"use client";
 
 import Files from "@/components/files";
 import Header from "@/components/header";
 import { useState } from "react";
 
-
 export default function Home() {
-
   const [filterString, setfilterString] = useState("");
   return (
     <div className="h-screen">
-      <Header filterString={filterString} setfilterString = {setfilterString}/>
-      <Files filter={filterString}/>
+      <Header filterString={filterString} setfilterString={setfilterString} />
+      <Files filter={filterString} />
     </div>
   );
 }
--- components/files.jsx
+++ components/files.jsx
@@ -1,19 +1,107 @@
-import filesData from "./tempFileData";
+// files.jsx
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { defaultStyles, FileIcon } from "react-file-icon";
 
 export default function Files({ filter }) {
+  const [filesData, setFilesData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("/api/fileNames");
+        setFilesData(response.data.files);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+
+    console.log("bool changed");
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  async function handleDownload(filename) {
+    try {
+      const response = await fetch(
+        `http://localhost:4000/getfile?fileName=${filename}`
+      );
+      if (!response.ok) {
+        alert("Sorry can download this file!");
+        return;
+      }
+      const blob = await response.blob();
+      const url = window.URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = filename; // This attribute will prompt the "Save As" dialog
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+      window.URL.revokeObjectURL(url);
+    } catch (error) {
+      console.error("Error downloading file", error);
+    }
+  }
+  function getExtension(filename) {
+    return filename.slice(((filename.lastIndexOf(".") - 1) >>> 0) + 2);
+  }
+
   return (
-    <div className="m-4 p-2 border-2 border-gray-500 h-4/5 overflow-y-scroll">
-      {filesData.map(
-        (filename, index) =>
-          filename.toLowerCase().includes(filter.toLowerCase()) && (
-            <div key={index} className="">
-              <div className="text-2xl">{filename}</div>
-              <svg height="20">
-                <line x1="0" y1="10" x2="200" y2="10" stroke="white" />
-              </svg>
-            </div>
-          )
-      )}
+    <div className="grid grid-cols-3 m-[5%] gap-40 place-items-center p-20">
+      {filesData.map((filename, index) => {
+        if (filename.toLowerCase().includes(filter.toLowerCase())) {
+          const extension = getExtension(filename);
+          return (
+            <button
+              key={index}
+              onClick={() => handleDownload(filename)}
+              className="flex flex-col items-center h-40 w-52 py-4 hover:bg-slate-400 rounded-lg"
+            >
+              <FileIcon extension={extension} {...defaultStyles[extension]} />
+
+              <p className="cursor-pointer mb-4 text-2xl font-bold">
+                {filename}
+              </p>
+            </button>
+          );
+        }
+
+        return null;
+      })}
     </div>
   );
+
+  // return (
+  //   <div className="m-4 p-2 border-2 border-gray-500 h-4/5 overflow-y-scroll">
+  //     {filesData
+  //       .filter((filename) =>
+  //         filename.toLowerCase().includes(filter.toLowerCase())
+  //       )
+  //       .map((filename, index) => (
+  //         <div key={index} className="">
+  //           <button
+  //             onClick={() => handleDownload(filename)}
+  //             className="text-2xl cursor-pointer hover:bg-white flex flex-col p-4 rounded-md"
+  //           >
+  //             {filename}
+  //           </button>
+  //           <svg height="20">
+  //             <line x1="0" y1="10" x2="200" y2="10" stroke="white" />
+  //           </svg>
+  //         </div>
+  //       ))}
+  //   </div>
+  // );
 }
